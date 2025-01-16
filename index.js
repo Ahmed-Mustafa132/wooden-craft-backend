248c87e9ff864a8bacc9ca0374b66623b72891d1
Ahmed Mostafa Ahmed Abbas <ahmedmustafa0384@gmail.com>
2025-01-17 00:18:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 app.use(express.json());
 
 // Connect to MongoDB
-const mongoURI = process.env.mongoURI  ;
+const mongoURI = process.env.mongoURI ;
 mongoose.connect(mongoURI,{useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log('MongoDB connection error:', err));
